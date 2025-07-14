0e67f4d18699eaa639dcfac42bc7a292873ca505
sdcote <sdcote@gmail.com>
2025-07-14 12:45:33 -0400

    Initial structure

---
+++ src/test/java/com/sdcote/profilesvc/ProfilesvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdcote.profilesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
